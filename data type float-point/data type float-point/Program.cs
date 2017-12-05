88542e66452e7ef10ccaed32ccbf446956e8bc42
samihsn <samihsn@gmail.com>
2017-12-05 09:38:38 +0300

    variable (float-point
    float taype , double  , decimal

---
+++ data type float-point/data type float-point/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace data_type_float_point
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float flMin = float.MinValue;
+            float flMax = float.MaxValue;
+
+
+            Console.WriteLine("Min value  = (0) Max val = (1)", flMin, flMax);
+            Console.ReadKey();
+        }
+    }
+}
